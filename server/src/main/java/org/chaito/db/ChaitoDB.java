203f10781d2dd1c360c1321cefa7fdb0bcb3db8f
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-02-21 16:57:39 +0100

    DB created to store msgs

--- server/src/main/java/org/chaito/db/ChaitoDB.java
+++ server/src/main/java/org/chaito/db/ChaitoDB.java
@@ -1,5 +1,7 @@
 package org.chaito.db;
 
+import org.chaito.ClientThread;
+import org.chaito.Server;
 import org.chaito.model.Msg;
 
 import java.util.ArrayList;
@@ -11,15 +13,26 @@ public class ChaitoDB extends AccessDB {
         super(FILE_PATH);
     }
 
+    public ArrayList<Msg> getAll() {
+        String query = "SELECT * FROM MSGS;";
+        return sqlite2msg(SQLiteQuery.get(this, 4, query));
+    }
+
+    public ArrayList<Msg> getMsgs(String target) {
+        String query = "SELECT * FROM MSGS WHERE target_user = ? or target_user = '" + Server.ALL_TARGET + "' or sender_user = ?;";
+        return sqlite2msg(SQLiteQuery.get(this, 4, query, target, target));
+    }
+
+    public int insert(Msg msg) {
+        String query = "INSERT INTO MSGS (target_user, sender_user, msg) VALUES (?, ?, ?);";
+        return SQLiteQuery.execute(this, query, msg.getSender(), msg.getTarget(), msg.getMsg());
+    }
+
     private ArrayList<Msg> sqlite2msg(ArrayList<Object[]> data) {
         ArrayList<Msg> msgs = new ArrayList<>();
         for (Object[] row : data) {
-            msgs.add(new Msg((String) row[0], (String) row[1], (String) row[2]));
+            msgs.add(new Msg((String) row[1], (String) row[2], (String) row[3]));
         }
         return msgs;
-    }
-
-    public static void main(String[] args) {
-        ChaitoDB db = new ChaitoDB();
     }
 }
